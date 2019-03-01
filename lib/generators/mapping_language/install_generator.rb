6060e97a268f99488d4364fdd02204626f9ca8c1
范智锋 <gzfanzhifeng@corp.netease.com>
2019-03-01 23:49:32 +0800

    兼容问题

--- lib/generators/mapping_language/install_generator.rb
+++ lib/generators/mapping_language/install_generator.rb
@@ -1,19 +1,36 @@
 require 'rails/generators'
 require 'rails/generators/migration'
+require 'rails/generators/active_record'
+
 module MappingLanguage
   module Generators
-    class InstallGenerator < Rails::Generators::Base
-      desc 'generate base files'
-      source_root File.expand_path('../../templates', __dir__)
+    class InstallGenerator < ActiveRecord::Generators::Base
+      include Rails::Generators::Migration
+      source_root File.expand_path('../templates', __dir__)
+
+      desc 'Generate Required files'
 
       def create_migrations
-        unless options[:skip_comments]
+        unless migration_file_exists?('create_lang_mappings')
           migration_template 'migrations/create_lang_mappings.rb.erb', 'db/migrate/create_lang_mappings.rb'
+        end
+
+        unless migration_file_exists?('add_column_language_to_user')
+          migration_template 'migrations/add_column_language_to_user.rb.erb', 'db/migrate/add_column_language_to_user.rb'
         end
       end
 
       def create_model
         template 'models/lang_mapping.rb.erb', 'app/models/lang_mapping.rb'
+      end
+
+      def create_config
+        template 'config/extensions.rb', 'config/initializers/extensions.rb'
+      end
+
+      private
+      def migration_file_exists?(file_name)
+        Dir.glob("#{File.join(destination_root, "db", "migrate")}/[0-9]*_*.rb").grep(/\d+_#{file_name}.rb/).first
       end
     end
   end
